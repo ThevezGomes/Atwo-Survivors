99540d35c90996c9281dd7f350e07bcc9c80e981
G4me0ver23 <carlitosdellima@gmail.com>
2024-11-08 19:59:03 -0300

    Melhorias do código

--- mapa/codigo/Mapa_lp.py
+++ mapa/codigo/Mapa_lp.py
@@ -2,16 +2,39 @@ import pygame as pg
 import sys
 from pytmx.util_pygame import load_pygame
 
+class Tile(pg.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = image
+        self.rect = self.image.get_rect(topleft = pos)
+
+
+
 pg.init()
 screen = pg.display.set_mode((1280,720))
 tmx_data = load_pygame('../tmx/Mapa_lp_tiro.tmx')
-print(dir(tmx_data))
+sprite_group = pg.sprite.Group()
+
+#para mostrar os tile sets de chão
+for layer in tmx_data.visible_layers:
+    if hasattr(layer,'data'):
+        for x,y,surf in layer.tiles():
+            pos = (x * 32, y * 32)
+            Tile(pos = pos,surf = surf, groups= sprite_group)
+
+for obj in tmx_data.objects:
+    pos = obj.x, obj.y
+    if obj.type in ("Vegetacao","Pedras"):
+        Tile(pos = pos, surf = obj.image, groups = sprite_group)
+            
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            pg.QUIT()
+            pg.quit()
             sys.exit()
 
-            screen.fill('black')
-            pg.display.update()
+        screen.fill('black')
+        sprite_group.draw(screen)
+        pg.display.update()
+        
