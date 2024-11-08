7dad7ae92daf45cb8fe12520a3b031aeb92da168
G4me0ver23 <carlitosdellima@gmail.com>
2024-11-08 19:24:37 -0300

    Colocando dados do mapa provisório, atalizações necessárias

---
+++ mapa/tsx/OuterTileset_land_new.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="OuterTileset_land_new" tilewidth="32" tileheight="32" tilecount="224" columns="28">
+ <image source="../tilesets/OuterTileset_land.png" width="927" height="256"/>
+ <wangsets>
+  <wangset name="Terreno" type="mixed" tile="-1">
+   <wangcolor name="Brick" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="2,2,0,0,0,2,2,2"/>
+   <wangtile tileid="1" wangid="2,2,0,0,0,0,0,2"/>
+   <wangtile tileid="2" wangid="2,2,2,2,0,0,0,2"/>
+   <wangtile tileid="28" wangid="0,0,0,0,0,2,2,2"/>
+   <wangtile tileid="30" wangid="0,2,2,2,0,0,0,0"/>
+   <wangtile tileid="56" wangid="0,0,0,2,2,2,2,2"/>
+   <wangtile tileid="57" wangid="0,0,0,2,2,2,0,0"/>
+   <wangtile tileid="58" wangid="0,2,2,2,2,2,0,0"/>
+   <wangtile tileid="84" wangid="2,2,1,2,2,2,1,2"/>
+   <wangtile tileid="85" wangid="2,2,1,2,2,2,1,2"/>
+   <wangtile tileid="86" wangid="2,2,1,2,2,2,1,2"/>
+   <wangtile tileid="112" wangid="2,2,1,2,1,2,1,2"/>
+   <wangtile tileid="113" wangid="1,2,1,2,1,2,2,2"/>
+   <wangtile tileid="115" wangid="1,2,1,2,1,2,1,2"/>
+   <wangtile tileid="140" wangid="1,2,1,2,2,2,1,2"/>
+   <wangtile tileid="141" wangid="1,2,2,2,1,2,1,2"/>
+   <wangtile tileid="168" wangid="1,1,1,2,1,2,1,1"/>
+   <wangtile tileid="169" wangid="1,2,1,2,1,1,1,1"/>
+   <wangtile tileid="170" wangid="2,2,1,2,1,2,2,2"/>
+   <wangtile tileid="171" wangid="2,2,2,2,1,2,1,2"/>
+   <wangtile tileid="196" wangid="1,2,1,1,1,1,1,2"/>
+   <wangtile tileid="197" wangid="1,1,1,1,1,2,1,2"/>
+   <wangtile tileid="198" wangid="1,2,1,2,2,2,2,2"/>
+   <wangtile tileid="199" wangid="1,2,2,2,2,2,1,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
