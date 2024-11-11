b2684878c1ee234af9b63709a9e8dd20dce8b57a
José Thevez Gomes Guedes <162639354+ThevezGomes@users.noreply.github.com>
2024-11-10 21:28:09 -0300

    Adicionei comentarios extras

--- src/config.py
+++ src/config.py
@@ -1,12 +1,18 @@
+# Variavel que armazena o tamanho de certos objetos
 tilesize = 1
 
+# Variavel que define as camadas dos objetos
 player_layer = 2
 block_layer = 1
 
+# Variavel que define a velocidade do jogador
 player_speed = 7
 
+# Cor vermelha em rgb
 red = (255, 0 , 0)
 
+# Largura do personagem principal
 width = 88
 
+# Tamanho do personagem principal
 size = (width, (width*32)/22)
