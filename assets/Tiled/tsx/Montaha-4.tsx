617a482a4e56d677c728f98a01cba1171449c2a8
Davifvnunes <Davifvnunes1@gmail.com>
2024-11-18 21:26:47 -0300

    Adcionando arquivo tsx e melhorando o visual do mapa

---
+++ assets/Tiled/tsx/Montaha-4.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Montaha-4" tilewidth="3200" tileheight="1800" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../img-assets/Mountain scene Pink Clouds3.png" width="3200" height="1800"/>
+ </tile>
+ <tile id="1">
+  <image source="../img-assets/Mountain-Dusk.png" width="320" height="240"/>
+ </tile>
+ <tile id="2">
+  <image source="../img-assets/Mountain-Dusk_without_moon.png" width="320" height="240"/>
+ </tile>
+</tileset>
