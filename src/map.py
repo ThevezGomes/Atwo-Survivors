8e576de8d791d8ac4cb33fd692037b69e4199ec0
Davifvnunes <Davifvnunes1@gmail.com>
2024-12-04 22:16:11 -0300

    Modificação do doc e limpando o código

--- src/map.py
+++ src/map.py
@@ -29,67 +29,11 @@ class Tile(pg.sprite.Sprite):
         """
         Redimensiona o tile para uma nova largura e altura.
 
-        Args:
+        Argumentoss:
             width (int): Nova largura do tile.
             height (int): Nova altura do tile.
         """
         self.image = pg.transform.scale(self.image, (width, height))
         self.rect = self.image.get_rect(topleft=self.rect.topleft)
 
-    def transform_tile_image(tile_image, gid):
-        """
-        Aplica transformações na imagem do tile com base no GID (Global ID).
-
-        As transformações incluem:
-        - Virar horizontalmente.
-        - Virar verticalmente.
-        - Rotação diagonal (90 graus).
-
-        Argumentos:
-            tile_image (pygame.Surface): Superfície gráfica do tile.
-            gid (int): Global ID do tile, contendo informações de transformação.
-
-        Retorna:
-            pygame.Surface: Imagem transformada.
-        """
-        # Extraindo as flags de transformação dos bits do GID
-        flipped_horizontally = bool(gid & 0x80000000)  # Verifica o bit mais à esquerda (flip horizontal)
-        flipped_vertically = bool(gid & 0x40000000)    # Verifica o bit para flip vertical
-        flipped_diagonally = bool(gid & 0x20000000)    # Verifica o bit para rotação diagonal
-
-        # Cria a instância de TileFlags com as flags extraídas
-        tile_flags = pytmx.TileFlags(flipped_horizontally, flipped_vertically, flipped_diagonally)
-
-        # Aplica as transformações usando as flags
-        if tile_flags.flipped_horizontally or tile_flags.flipped_vertically:
-            tile_image = pg.transform.flip(tile_image, tile_flags.flipped_horizontally, tile_flags.flipped_vertically)
-
-        if tile_flags.flipped_diagonally:
-            tile_image = pg.transform.rotate(tile_image, 90)
-
-        return tile_image
-
-    def transform_object_image(obj, gid, offset_x, offset_y):
-        """
-        Aplica transformações na imagem associada a um objeto do mapa.
-
-        As transformações incluem ajustes de posição e rotação com base nas flags do GID.
-
-        Argumentoss:
-            obj (pytmx.TiledObject): Objeto do mapa contendo a imagem e informações de posicionamento.
-            gid (int): Global ID do objeto, contendo informações de transformação.
-            offset_x (int): Deslocamento horizontal para centralizar o objeto no mapa.
-            offset_y (int): Deslocamento vertical para centralizar o objeto no mapa.
-
-        Retorna:
-            Tile: atribuição do Tile representando o objeto transformado.
-        """
-        # Aplica transformações na imagem associada ao objeto
-        obj_image = Tile.transform_tile_image(obj.image, gid)
-
-        # Calcula a posição do objeto com o offset
-        pos = (obj.x - offset_x, obj.y - offset_y)
-
-        # Cria o sprite de objeto
-        tiled_obj_sprite = Tile(pos, obj_image, [self.all_sprites])
-        return tiled_obj_sprite
+    
