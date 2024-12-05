e41ed46353557740c0b0e2b29d46bc53f9ea421e
Henrique Gasparelo <henriquegasparelo@gmail.com>
2024-12-04 22:01:19 -0300

    Documentação do main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
+"""Módulo principal do jogo, que inicializa e executa a aplicação do jogo."""
+
 import pygame
 from game import *
 
+# Inicializa uma nova instância do jogo
 g = Game()
-g.intro_screen()
-while g.running:
-    g.run()
-    if g.restart:
-        pygame.mixer.stop()
-        g = Game()
-        pygame.time.delay(150)
-        g.intro_screen()
+g.intro_screen()  # Exibe a tela de introdução do jogo
 
+# Executa o loop principal do jogo até que a variável `running` seja False
+while g.running:
+    g.run()  # Executa o loop principal do jogo
+    if g.restart:  # Verifica se o jogo deve ser reiniciado
+        pygame.mixer.stop()  # Para a música e efeitos de áudio
+        g = Game()  # Cria uma nova instância do jogo
+        pygame.time.delay(150)  # Aguarda um curto intervalo para reiniciar
+        g.intro_screen()  # Exibe a tela de introdução novamente
+
+# Finaliza o Pygame e sai do programa
 pygame.quit()
 sys.exit()
